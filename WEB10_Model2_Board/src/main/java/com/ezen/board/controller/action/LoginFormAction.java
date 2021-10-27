0d47ce4ffda0c03ee99bdf6a6fb78cdc82f46a02
YataNox <rladnwls0523@naver.com>
2021-10-27 20:53:14 +0900

    Write execute Method

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/LoginFormAction.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/LoginFormAction.java
@@ -2,14 +2,23 @@ package com.ezen.board.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginFormAction implements Action {
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "member/loginForm.jsp";
 		
+		HttpSession session = request.getSession(); // 페이지의 세션 추출
+		if(session.getAttribute("loginUser") != null)
+			url = "main.jsp";
+		
+		RequestDispatcher dp = request.getRequestDispatcher(url);
+		dp.forward(request, response);
 	}
 
 }
