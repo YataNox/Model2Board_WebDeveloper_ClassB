3038af0745b0d3fa7e7caca1b065eaa9ba538d98
YataNox <rladnwls0523@naver.com>
2021-10-28 23:15:31 +0900

    Write EditMemberFormAction.java

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/EditMemberFormAction.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/EditMemberFormAction.java
@@ -2,12 +2,14 @@ package com.ezen.board.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class EditMemberFormAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		RequestDispatcher dp = request.getRequestDispatcher("member/editMemberForm.jsp");
+		dp.forward(request, response);
 	}
 }
