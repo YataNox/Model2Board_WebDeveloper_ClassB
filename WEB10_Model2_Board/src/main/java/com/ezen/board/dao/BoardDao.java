727d6d9aa8061c92ca08f40395c0e58cac101bad
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-10-28 18:18:09 +0900

    Write selectBoard Method

--- WEB10_Model2_Board/src/main/java/com/ezen/board/dao/BoardDao.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/dao/BoardDao.java
@@ -3,6 +3,11 @@ package com.ezen.board.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.ezen.board.dto.BoardDto;
+import com.ezen.board.util.DBman;
 
 public class BoardDao {
 	private BoardDao() {}
@@ -12,4 +17,34 @@ public class BoardDao {
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	
+	public ArrayList<BoardDto> selectBoard() 
+	{
+		ArrayList<BoardDto> list = new ArrayList<BoardDto>();
+		con = DBman.getConnection();
+		String sql = "select * from board order by num desc";
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				BoardDto bdto = new BoardDto();
+				bdto.setNum(rs.getInt("num"));
+				bdto.setUserid(rs.getString("userid"));
+				bdto.setPass(rs.getString("pass"));
+				bdto.setEmail(rs.getString("email"));
+				bdto.setTitle(rs.getString("title"));
+				bdto.setContent(rs.getString("content"));
+				bdto.setReadcount(rs.getInt("readcount"));
+				bdto.setWritedate(rs.getTimestamp("writedate"));
+				
+				list.add(bdto);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
 }
