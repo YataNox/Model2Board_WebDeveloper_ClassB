3413d93d3d9392e77c0c1d7549795cadec0fe963
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-10-28 17:22:35 +0900

    Write not exist user case

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/LoginAction.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/LoginAction.java
@@ -2,6 +2,7 @@ package com.ezen.board.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,5 +17,14 @@ public class LoginAction implements Action {
 		
 		MemberDao mdao = MemberDao.getInstance();
 		MemberDto mdto = mdao.getMember(userid);
+		
+		String url = "member/loginForm.jsp";
+		
+		if(mdto == null) {
+			request.setAttribute("message", "존재하지 않는 아이디입니다.");
+		}
+		
+		RequestDispatcher dp = request.getRequestDispatcher(url);
+		dp.forward(request, response);
 	}
 }
