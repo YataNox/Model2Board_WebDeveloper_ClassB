62b7007d885100ee2b7ef916692f38beb0341c2e
YataNox <rladnwls0523@naver.com>
2021-10-29 20:21:03 +0900

    Add command else if case - boardWriteForm, boardWrite

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/ActionFactory.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/ActionFactory.java
@@ -2,6 +2,8 @@ package com.ezen.board.controller;
 
 import com.ezen.board.controller.action.Action;
 import com.ezen.board.controller.action.BoardListAction;
+import com.ezen.board.controller.action.BoardWriteAction;
+import com.ezen.board.controller.action.BoardWriteFormAction;
 import com.ezen.board.controller.action.EditMemberAction;
 import com.ezen.board.controller.action.EditMemberFormAction;
 import com.ezen.board.controller.action.IdCheckAction;
@@ -28,7 +30,8 @@ public class ActionFactory {
 		else if(command.equals("editMemberForm")) ac = new EditMemberFormAction();
 		else if(command.equals("editMember")) ac= new EditMemberAction();
 		else if(command.equals("logout")) ac = new LogoutAction();
-		
+		else if(command.equals("boardWriteForm")) ac = new BoardWriteFormAction();
+		else if(command.equals("boardWrite")) ac = new BoardWriteAction();
 		return ac;
 	}
 }
