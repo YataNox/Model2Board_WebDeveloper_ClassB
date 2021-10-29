82a6e6aafb54df2005560f455200584396508f7b
YataNox <rladnwls0523@naver.com>
2021-10-29 18:26:22 +0900

    Update JoinAction.java -send data code

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/JoinAction.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/JoinAction.java
@@ -22,15 +22,18 @@ public class JoinAction implements Action {
 		mdto.setAdmin(Integer.parseInt(request.getParameter("admin")));
 		MemberDao mdao = MemberDao.getInstance();
 		
-		int result = mdao.insertMember(mdto);
+		/*int result =*/ mdao.insertMember(mdto);
+		// String message = "";
 		
-		if(result == 1) {
-			request.setAttribute("message", "회원가입 완료. 로그인하세요.");
-		}else {
-			request.setAttribute("message", "회원가입 실패. 관리자에게 문의하세요");
-		}
+		/*
+		 * if(result == 1) { message = "회원가입 완료. 로그인하세요."; }else { message =
+		 * "회원가입 실패. 관리자에게 문의하세요"; }
+		 */
 
-		RequestDispatcher dp = request.getRequestDispatcher("member/loginForm.jsp");
-		dp.forward(request, response);
+		// RequestDispatcher dp = request.getRequestDispatcher("member/loginForm.jsp");
+		// dp.forward(request, response);
+		// 최종 도착 페이지에서 새로고침을 하면 데이터도 한 번 더 추가되려고 시도합니다.
+		// 새로고침에 의해 포워딩 이전 코드가 다시 실행되지 않으려면 sendRedirect를 이용합니다.
+		response.sendRedirect("board.do?command=loginForm");
 	}
 }
