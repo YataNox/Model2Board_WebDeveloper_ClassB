6bd3a6f1533612432a1f904706545dfab0d1a1ae
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-01 18:18:15 +0900

    Write annotation

--- WEB10_Model2_Board/src/main/webapp/script/board.js
+++ WEB10_Model2_Board/src/main/webapp/script/board.js
@@ -1,3 +1,4 @@
+/* 로그인 확인 function */
 function loginCheck(){
 	if(document.frm.userid.value.length==0){
 		alert("아이디를 입력하세요.");
@@ -12,6 +13,7 @@ function loginCheck(){
 	return true;
 }
 
+/* 중복체크 팝업 창 function */
 function idCheck(){
 	if(document.frm.userid.value == ""){
 		alert("아이디를 입력해주세요.");
@@ -23,7 +25,8 @@ function idCheck(){
 	var opt = "toolbar=no, menubar=no, scrollbar=yes, resizable=no, width=500, height=200";
 	window.open("board.do?command=idcheck&userid=" + inputid, "idcheck", opt);
 }
-			
+
+/* 회원가입 사항 체크 function */
 function joinCheck(){
 	if(document.frm.userid.value.length==0){
 		alert("아이디는 필수 입력 사항입니다.");
@@ -53,6 +56,7 @@ function joinCheck(){
 	return true;
 }
 
+/* 아이디 중복체크 시 작동할 기능 function */
 function idok(userid){
 	// 현재 팝업창의 아이디 입력란에 있는 아이디를 오프너의 아이디 입력란에 복사합니다.
 	opener.document.frm.userid.value = userid;
@@ -62,6 +66,7 @@ function idok(userid){
 	self.close();
 }
 
+/* 회원 정보 수정 시 필수 입력사항 확인 function */
 function editCheck(){
 	if(document.frm.name.value==""){
 		alert("이름은 필수 입력 사항입니다.");
@@ -82,6 +87,7 @@ function editCheck(){
 	return true;
 }
 
+/* 게시물 작성시 필수 작성요소 확인 function */
 function boardCheck(){
 	if(document.frm.pass.value==""){
 		alert("비밀번호는 수정/삭제시 필요합니다.");
@@ -101,14 +107,14 @@ function boardCheck(){
 	return true;
 }
 
-/* 게시물 수정 전 비밀번호 확인 팝업창 표시 function*/
+/* 게시물 수정/삭제 전 비밀번호 확인 팝업창 표시 function*/
 function open_win(num, name){
 	var url = "board.do?command=boardPassForm&num=" + num;
 	var opt = "toolbar=no, menubar=no, scrollbar=yes, resizable=no, width=500, height=300";
 	window.open(url, name, opt);
 }
 
-/*게시물 수정 비밀번호 확인 funtion*/
+/*게시물 수정/삭제 비밀번호 확인 funtion*/
 function passCheck(){
 	if(document.frm.pass.value.length==0){
 		alert("비밀번호를 입력하세요");
