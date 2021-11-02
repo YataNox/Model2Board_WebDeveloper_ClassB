355b0773cef5c193d94ce1ced6dd166baec1414f
YataNox <rladnwls0523@naver.com>
2021-11-02 16:42:01 +0900

    Add replylist call code

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/BoardViewAction.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/BoardViewAction.java
@@ -1,6 +1,7 @@
 package com.ezen.board.controller.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.ezen.board.dao.BoardDao;
 import com.ezen.board.dto.BoardDto;
+import com.ezen.board.dto.ReplyDto;
 
 public class BoardViewAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,8 +22,12 @@ public class BoardViewAction implements Action {
 		// 게시물 번호를 이용하여 게시물 하나를 조회하고 리턴 받습니다.
 		BoardDto bdto = bdao.getBoard(num);
 		
+		// 해당 게시물의 댓글을 게시물 번호로 조회하여 리스트로 리턴 받습니다.
+		ArrayList<ReplyDto> list  = bdao.selectReply(num);
+		
 		// 리턴받은 게시글을 request에 저장하고 boardView.jsp로 포워딩합니다.
 		request.setAttribute("board", bdto);
+		request.setAttribute("replyList", list);
 		
 		RequestDispatcher dp = request.getRequestDispatcher("board/boardView.jsp");
 		dp.forward(request, response);
