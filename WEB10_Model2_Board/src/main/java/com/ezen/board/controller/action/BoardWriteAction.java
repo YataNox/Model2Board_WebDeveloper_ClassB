d13824c76dc8646386bc8b20445ab0c91551f92e
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-03 20:34:15 +0900

    Update code Multi form

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/BoardWriteAction.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/BoardWriteAction.java
@@ -2,25 +2,42 @@ package com.ezen.board.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.ezen.board.dao.BoardDao;
 import com.ezen.board.dto.BoardDto;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class BoardWriteAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 전달된 내용들을 BoardDto에 담아서 BoardDao의 insertBoard메소드로
 		// 전달해서 레코드를 추가합니다.
-		
+		request.setCharacterEncoding("UTF-8");
 		BoardDto bdto = new BoardDto();
-		bdto.setUserid(request.getParameter("userid"));
-		bdto.setPass(request.getParameter("pass"));
-		bdto.setEmail(request.getParameter("email"));
-		bdto.setTitle(request.getParameter("title"));
-		bdto.setContent(request.getParameter("content"));
 		
+		HttpSession session = request.getSession();
+		ServletContext context = session.getServletContext();
+		String path = context.getRealPath("upload");
+		
+		try {
+		MultipartRequest multi = new MultipartRequest(request,
+				path, 5*1024*1024, "UTF-8",
+				new DefaultFileRenamePolicy());
+		
+		bdto.setUserid(multi.getParameter("userid"));
+		bdto.setPass(multi.getParameter("pass"));
+		bdto.setEmail(multi.getParameter("email"));
+		bdto.setTitle(multi.getParameter("title"));
+		bdto.setContent(multi.getParameter("content"));
+		bdto.setImgfilename(multi.getFilesystemName("imgfilename"));
+		}catch(Exception e) {
+			System.out.println("업로드 실패 : " + e);
+		}
 		BoardDao bdao = BoardDao.getInstance();
 		bdao.insertBoard(bdto);
 		
