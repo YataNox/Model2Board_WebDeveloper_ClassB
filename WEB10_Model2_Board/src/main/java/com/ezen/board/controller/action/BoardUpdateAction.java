862a441cb702097298c3510e3f334d5dbbab1f3b
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-03 21:01:37 +0900

    Update code - MultipartRequest

--- WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/BoardUpdateAction.java
+++ WEB10_Model2_Board/src/main/java/com/ezen/board/controller/action/BoardUpdateAction.java
@@ -3,24 +3,41 @@ package com.ezen.board.controller.action;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.ezen.board.dao.BoardDao;
 import com.ezen.board.dto.BoardDto;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class BoardUpdateAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String num = request.getParameter("num");
-		
+		request.setCharacterEncoding("UTF-8");
+		String num = "";
+		HttpSession session = request.getSession();
+		ServletContext context = session.getServletContext();
+		String path = context.getRealPath("upload");
 		BoardDto bdto = new BoardDto();
-		bdto.setUserid(request.getParameter("userid"));
-		bdto.setPass(request.getParameter("pass"));
-		bdto.setEmail(request.getParameter("email"));
-		bdto.setTitle(request.getParameter("title"));
-		bdto.setContent(request.getParameter("content"));
-		bdto.setNum(Integer.parseInt(request.getParameter("num")));
+		
+		try {
+			MultipartRequest multi = new MultipartRequest(request,
+					path, 5*1024*1024, "UTF-8",
+					new DefaultFileRenamePolicy());
+			num = multi.getParameter("num");
+			bdto.setUserid(multi.getParameter("userid"));
+			bdto.setPass(multi.getParameter("pass"));
+			bdto.setEmail(multi.getParameter("email"));
+			bdto.setTitle(multi.getParameter("title"));
+			bdto.setContent(multi.getParameter("content"));
+			bdto.setNum(Integer.parseInt(multi.getParameter("num")));
+			bdto.setImgfilename(multi.getFilesystemName("imgfilename"));
+		}catch(Exception e) {
+			System.out.println("업로드 실패 : " + e);
+		}
 	
 		BoardDao bdao = BoardDao.getInstance();
 		bdao.updateBoard(bdto);
